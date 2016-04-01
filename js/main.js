da5272e1c3a36e9a8c345d6f739d3d0517e58dff
Ann Pavlova <ann_pavlova@outlook.com>
2016-04-01 21:54:15 +0300

    Update website

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
-jQuery(document).ready(function($){
+$(document).ready(function(){
+
+    var lastId,
+        topMenu = $("header"),
+        topMenuHeight = 75,
+        menuItems = topMenu.find("a"),
+        scrollItems = menuItems.map(function(){
+            var item = $($(this).attr("href"));
+            if (item.length) { return item; }
+        });
 
 /*===================
  //FIXED MENU
@@ -9,51 +18,26 @@ jQuery(document).ready(function($){
     });
 
 /*===================
- //NAVIGATION LINE
+ //SCROLL AND TARGET
  ===================*/
 
-//click-target
-    $(document).ready(function() {
-        $('.menu a[href*=#]').bind('touchstart click', function(e) {
-            e.preventDefault();
+    $('.menu a[href*=#]').bind('touchstart click', function(e) {
+        var target = $(this).attr("href");
 
-            var target = $(this).attr("href");
-            var hh = $(".header").height();
+        var targetTopOffset = $(target).offset().top;
+        if(Math.round(targetTopOffset) ==  $(document).scrollTop()) {return false;}
 
-            $('html, body').stop().animate({ scrollTop: $(target).offset().top - hh}, 500, function() {
-                location.hash = target;
-            });
-            $('.navbar-toggle:visible').click();
-            return true;
-        });
-    });
+        var x = targetTopOffset - topMenuHeight;
+        $('html, body').stop().animate({ scrollTop: x}, 400);
+        $('.navbar-toggle:visible').click();
 
-//scroll-target
-    var lastId,
-        topMenu = $("header"),
-        topMenuHeight = topMenu.outerHeight()+15,
-    // All list items
-        menuItems = topMenu.find("a"),
-    // Anchors corresponding to menu items
-        scrollItems = menuItems.map(function(){
-            var item = $($(this).attr("href"));
-            if (item.length) { return item; }
-        });
-
-
-    menuItems.on('touchstart click', function(e){
-        var href = $(this).attr("href"),
-            offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight;
-        $('html, body').stop().animate({
-            scrollTop: offsetTop
-        }, 300);
         e.preventDefault();
+        return false;
     });
-
 
     $(window).scroll(function(){
         // Get container scroll position
-        var fromTop = $(this).scrollTop()+topMenuHeight;
+        var fromTop = $(this).scrollTop() + topMenuHeight + 2;
 
         // Get id of current scroll item
         var cur = scrollItems.map(function(){
@@ -106,44 +90,17 @@ jQuery(document).ready(function($){
 
 
 /*===================
- //COUNTER
- ===================*/
-    var time = 2;
-    var cc = 1;
-    $(window).scroll(function(){
-        $('#counter').each(function(){
-            var
-                cPos = $(this).offset().top,
-                topWindow = $(window).scrollTop();
-            if(cPos < topWindow + 400) {
-                if (cc < 2) {
-                    $(".number").addClass("viz");
-                    $('span.number').each(function () {
-                        var
-                            i = 1,
-                            num = $(this).data('num'),
-                            step = 1000 * time / num,
-                            that = $(this),
-                            int = setInterval(function () {
-                                if (i <= num) {
-                                    that.html(i);
-                                }
-                                else {
-                                    cc = cc + 2;
-                                    clearInterval(int);
-                                }
-                                i++;
-                            }, step);
-                    });
-                }
-            }
-        });
-    });
-
-/*===================
  //PARALLAX
  ===================*/
-    $('#shell img').plaxify();
-    $.plax.enable();
+$('#shell img').plaxify();
+$.plax.enable();
 
+/*===================
+ //COUNTER
+ ===================*/
+
+    $('.counter').counterUp({
+        delay: 50,
+        time: 1500
+    });
 });
